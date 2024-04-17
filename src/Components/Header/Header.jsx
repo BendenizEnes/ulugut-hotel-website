8936e1eb493ab22745457cf91ef8de282074719e
BendenizsEnes <e.hacihuseyin03@gmail.com>
2024-04-17 18:58:22 +0300

    Header Updated

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,46 @@
 import Navbar from "./Navbar/Navbar.jsx";
-import Hero from "./Hero/Hero.jsx";
 import "./Header.css"
+import {useState, useEffect} from "react";
 
 function Header() {
+
+    const images = ['url("public/images/herobg1.jpg")','url("public/images/herobg2.jpg")']
+    const h2texts = ['Enjoy Your Holiday','MORE THAN A HOTEL...']
+    const h1texts = ['Refuel Your Energy and Discover the Beauties of Denizli','Hotel for the whole family, all year around']
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [backgroundImage, setBackgroundImage] = useState(images[currentIndex]);
+    const [h1Text, seth1Text] = useState(h1texts[currentIndex]);
+    const [h2Text, seth2Text] = useState(h2texts[currentIndex]);
+
+    const changeContent = () => {
+        const nextIndex = (currentIndex + 1) % images.length;
+        setCurrentIndex(nextIndex);
+        setBackgroundImage(images[nextIndex]);
+        seth1Text(h1texts[nextIndex]);
+        seth2Text(h2texts[nextIndex]);
+    };
+
+    useEffect(() => {
+        const interval = setInterval(changeContent, 10000);
+        return () => clearInterval(interval);
+    }, [currentIndex]);
 return(
-    <div id="header" className="w-screen h-screen flex flex-row justify-center items-center">
+    <div id="header" className="" style={{
+        backgroundImage:backgroundImage,
+        backgroundSize:"cover",
+        backgroundRepeat:"no-repeat",
+        backgroundPosition:"center",
+        transition: 'background-image 1s ease-in-out',
+    }}>
         <Navbar/>
-        <Hero/>
+
+        <div id="content" className="z-10 text-white flex flex-row-reverse">
+            <div className="row w-1/2">
+                 <h2 className="text-2xl font-bold" style={{transition: 'opacity 1s ease-in-out'}}>{h2Text}</h2>
+                 <h1 className="text-6xl font-bold" style={{transition: 'opacity 1s ease-in-out'}}>{h1Text}</h1>
+            </div>
+        </div>
     </div>
 )
 }
--- src/Components/Header/Navbar/Navbar.jsx
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -3,14 +3,14 @@ const Navbar = () => {
     return (
         <div className="flex justify-around items-center bg-transparent text-white fixed top-0 left-0 w-screen pt-1">
             <div className="logo inline-block text-3xl font-bold">
-                <a href="/public">Ulugut Hotel</a>
+                <a href="/">Ulugut Hotel</a>
             </div>
             <div className="links inline-block">
                 <ul className="flex">
-                    <li className="pl-10 p-5"><a href="/public">Home</a></li>
-                    <li className="pl-10 p-5"><a href="/public">About</a></li>
-                    <li className="pl-10 p-5"><a href="/public">Our Rooms</a></li>
-                    <li className="pl-10 p-5"><a href="/public">Contact</a></li>
+                    <li className="pl-10 p-5"><a href="/">Home</a></li>
+                    <li className="pl-10 p-5"><a href="/">About</a></li>
+                    <li className="pl-10 p-5"><a href="/">Our Rooms</a></li>
+                    <li className="pl-10 p-5"><a href="/">Contact</a></li>
                 </ul>
             </div>
         </div>
