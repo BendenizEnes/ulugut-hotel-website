365be6515d7f18244a16a13272eac82a95ea9386
BendenizsEnes <e.hacihuseyin03@gmail.com>
2024-04-18 21:51:49 +0300

    Added Footer

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,6 +1,7 @@
 import Header from "../Components/Header/Header.jsx";
 import Features from "../Components/Features/Features.jsx";
 import Gallery from "../Components/Gallery/Gallery.jsx";
+import Footer from "../Components/Footer/Footer.jsx";
 function App() {
 
   return (
@@ -8,6 +9,7 @@ function App() {
         <Header/>
         <Features></Features>
         <Gallery></Gallery>
+        <Footer></Footer>
     </>
   )
 }
